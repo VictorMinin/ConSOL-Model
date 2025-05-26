0eb429b46f253334764f6ce42d1347ecf4486c26
Victor Minin <68088431+VictorMinin@users.noreply.github.com>
2025-05-26 13:56:21 -0400

    Adding Potentiometer Testing Script
    Use this script to experimentally determine the potentiometer values that are desired.

---
+++ Potentiometer_Test.ino
@@ -0,0 +1,20 @@
+/*
+  Run this script to experimentally determine the voltage values of the potentiometer once the desired extension of the actuator
+  is achieved
+  NOTE: my ADC equation transforming the analog value to digitial is only valid if using the boards 5V power
+  if another voltage source/ value is used to power the potentiometer then that value must be accounted for in the EQ.
+  
+  To set up the potentiometer correctly simply attach the boards ground to the potentiometer's ground, the 5v to the positive V on 
+  the potentiometer, and the A0 analog pin to the 3rd pin on the potentiometer.
+*/
+void setup() {
+  Serial.begin(9600)
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0 / 1023.0); // ADC EQ is V = Analog * (max Voltage/ bit resolution of ADC)
+  Serial.print("Current Voltage: ");
+  Serial.print(voltage);
+  Serial.println("V");
+}
